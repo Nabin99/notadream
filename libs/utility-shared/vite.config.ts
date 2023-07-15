f8a16f6374209e4a154b7b5fdbce2c5d808a659e
Nabin Dhital <dhitalnabin11@gmail.com>
2023-07-15 11:47:30 +0545

    refactor: clean mono repo structure

--- libs/utility-shared/vite.config.ts
+++ libs/utility-shared/vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "react-shared",
-      fileName: "react-shared",
+      name: "utility-shared",
+      fileName: "utility-shared",
     },
     rollupOptions: {
       external: [...Object.keys(devDependencies)],
