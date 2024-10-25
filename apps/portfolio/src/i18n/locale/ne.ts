f69235bfd23172eae2670da4b76b892a7dc3cdd8
Nabin Dhital <dhitalnabin11@gmail.com>
2024-10-25 13:27:46 -0400

    feat(libs/react/i18n): use standard locale code

---
+++ apps/portfolio/src/i18n/locale/ne.ts
@@ -0,0 +1,21 @@
+import type { TranslationData } from "@notadream/react";
+
+export const ne: TranslationData = {
+  app: {
+    name: "Nabin Dhital [ne]",
+    description: "A professional full stack developer [ne]",
+    title: "Nabin Dhital [ne]",
+  },
+  header: {
+    navigationList: {
+      home: "Home [ne]",
+      about: "About [ne]",
+      services: "Services [ne]",
+    },
+  },
+  footer: {
+    copyRight: "Copyright {{year}} {{name}}. All Rights Reserved. [ne]",
+    buildMessage: "Build from [ne]",
+    thankYouMessage: "Thanks for scrolling, that's all folks. [ne]",
+  },
+};
