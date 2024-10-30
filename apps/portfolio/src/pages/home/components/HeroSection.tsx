02b01bc1e02cb4d464ff3d4fc98c2d538d46a5c3
Nabin Dhital <dhitalnabin11@gmail.com>
2024-10-29 23:12:02 -0400

    feat(apps/portfolio/about): minor visual improvements

--- apps/portfolio/src/pages/home/components/HeroSection.tsx
+++ apps/portfolio/src/pages/home/components/HeroSection.tsx
@@ -10,18 +10,18 @@ export const HeroSection = () => {
   return (
     <section className="hero-section">
       <ContentBox>
-        <RippleEffect />
         <div className="heading">
           <span>{t("greetings")}</span>
           <p>{t("introduction")}</p>
           <h1>{t("title")}</h1>
           <p>{t("subtitle")}</p>
           <div className="hero-buttons">
-            <Button label={t("buttons.explore")} variant="outline" />
+            <Button label={t("buttons.explore")} variant="solid" />
             <Button label={t("buttons.connect")} variant="outline" />
           </div>
           <SocialLinks />
         </div>
+        <RippleEffect />
       </ContentBox>
     </section>
   );
