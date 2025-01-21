6ffd1b99a0ea2a11c1e85ed1bf902b9d8e0772a5
Nabin Dhital <dhitalnabin11@gmail.com>
2025-01-20 23:15:45 -0500

    feat(apps/portfolio): update package.json

--- apps/portfolio/vite.config.ts
+++ apps/portfolio/vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig(({ mode }) => {
         },
       }),
     ],
+    base: "/notadream/",
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
