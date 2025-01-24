8c6b00dd887f61357ce50e4369ebbf3d27311521
Nabin Dhital <dhitalnabin11@gmail.com>
2025-01-24 12:46:44 -0500

    feat(apps/portfolio): vite base path

--- apps/portfolio/vite.config.ts
+++ apps/portfolio/vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig(({ mode }) => {
         },
       }),
     ],
-    base: "/notadream/",
+    // base: "/notadream/",
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
