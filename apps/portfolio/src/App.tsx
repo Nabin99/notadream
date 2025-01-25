d8adb06d7f2c65eab728ce80a5e83882dec157c9
Nabin Dhital <dhitalnabin11@gmail.com>
2025-01-25 04:21:40 -0500

    feat(apps/portfolio): fix footer css, add support for multi color mode

--- apps/portfolio/src/App.tsx
+++ apps/portfolio/src/App.tsx
@@ -1,3 +1,5 @@
+import { getAppConfig } from "@notadream/react";
+import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 
 import { browserRouter } from "./Routes";
@@ -5,6 +7,40 @@ import { browserRouter } from "./Routes";
 import "@notadream/react/dist/index.css";
 
 function App() {
+  const appConfig = getAppConfig();
+
+  useEffect(() => {
+    const initialHue = 187;
+    const saturation = 79;
+    const lightness = 47;
+    let currentHue = initialHue;
+
+    const updatePrimaryColor: IdleRequestCallback = (deadline) => {
+      if (deadline.timeRemaining() > 0) {
+        currentHue = (currentHue + 1) % 360; // Increment and loop hue
+        const newColor = `hsl(${currentHue}, ${saturation}%, ${lightness}%)`;
+        (
+          document.getElementsByClassName("page-layout")[0] as HTMLElement
+        ).style.setProperty("--nd-colors-accent", newColor);
+      }
+      requestIdleCallback(updatePrimaryColor, {
+        timeout: 10000, // Increase timeout to decrease update frequency
+      });
+    };
+
+    let idleCallbackId: number;
+
+    if (appConfig.theme.multiColorMode) {
+      idleCallbackId = requestIdleCallback(updatePrimaryColor);
+    }
+
+    return () => {
+      if (appConfig.theme.multiColorMode) {
+        return cancelIdleCallback(idleCallbackId);
+      }
+    }; // Cleanup on unmount
+  }, []);
+
   return (
     <>
       <RouterProvider router={browserRouter} />
