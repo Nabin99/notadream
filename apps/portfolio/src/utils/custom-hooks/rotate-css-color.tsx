5cd4b7b6506518ea592b333493353d3ba5728d9d
Nabin Dhital <dhitalnabin11@gmail.com>
2025-02-19 17:29:18 -0500

    feat(apps/portfolio): update the update frequency, and css for the scroll to top

--- apps/portfolio/src/utils/custom-hooks/rotate-css-color.tsx
+++ apps/portfolio/src/utils/custom-hooks/rotate-css-color.tsx
@@ -1,7 +1,6 @@
 import { getAppConfig } from "@notadream/react";
 import { useEffect, useRef } from "react";
 
-
 export const useRotateCSSColorValue = () => {
   const appConfig = getAppConfig();
   const hueReference = useRef(187); // Store hue in a ref to avoid unnecessary re-renders
@@ -15,18 +14,16 @@ export const useRotateCSSColorValue = () => {
       const newColor = `hsl(${hueReference.current}, 79%, 47%)`;
 
       // Get element safely
-      const layoutElement = document.querySelector(
-        ".page-layout"
-      ) as HTMLElement;
+      const layoutElement = document.querySelector("body") as HTMLElement;
       if (layoutElement) {
         layoutElement.style.setProperty("--nd-colors-accent", newColor);
       }
 
       // Use setTimeout for better performance instead of requestAnimationFrame (adjust delay as needed)
-      frameReference.current = window.setTimeout(updatePrimaryColor, 100); // Update every 100ms
+      frameReference.current = window.setTimeout(updatePrimaryColor, 144); // Update every 100ms
     };
 
-    frameReference.current = window.setTimeout(updatePrimaryColor, 100);
+    frameReference.current = window.setTimeout(updatePrimaryColor, 144);
 
     return () => {
       if (frameReference.current !== null) {
